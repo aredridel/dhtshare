c9ea0991585915698e038738adac293d33df9686
Aria Stewart <aredridel@nbtsc.org>
2012-11-26 00:15:41 -0500

    Connect to multiple nodes, use a node for each interface, and use good error codes

--- bootstrap.js
+++ bootstrap.js
@@ -1,3 +1,5 @@
 var dht = require('kademlia');
 
 var node = new dht.KNode({ address: process.argv[2], port: parseInt(process.argv[3], 10) });
+
+console.log("listening on address", process.argv[2], "and port", process.argv[3]);
--- server.js
+++ server.js
@@ -2,34 +2,69 @@ var express = require('express');
 
 var dht = require('kademlia');
 
-var node = new dht.KNode({ address: process.argv[2], port: parseInt(process.argv[3], 10) });
+var config = require('./config.json');
 
-node.connect('127.0.0.1', 12345, function() {
+var os = require('os');
+
+
+var nodes = [];
+var node;
+var interfaces = os.networkInterfaces();
+
+for (var idx in interfaces) {
+    for (var x in interfaces[idx]) {
+        if (interfaces[idx][x].family != 'IPv4') continue;
+        console.log(interfaces[idx][x]);
+        createNode(interfaces[idx][x].address);
+    }
+}
+
+function createNode(address) {
+    nodes.push(node = new dht.KNode({ address: address, port: 12346 }));
+    for (var i in config.bootnodes) {
+        node.connect(config.bootnodes[i].address, config.bootnodes[i].port, onConnected);
+    }
+}
+
+var connected = false;
+
+function onConnected() {
+    if (connected) return;
+
     console.log('connected');
+    connected = true;
+}
 
-    var app = express();
+var app = express();
 
-    app.use(app.router);
+app.use(app.router);
 
-    app.get('/*', function(req, res) {
-        node.get(req.params[0], function(err, value) {
-            if (err) console.log(err);
+app.get('/*', function(req, res) {
+    node.get(req.params[0], function(err, value) {
+        if (err) {
+            res.statusCode = (err.code == 'NOTFOUND' ? 404 : 500);
+            res.send(JSON.stringify(err));
+        } else {
             res.send(value);
-        });
+        }
     });
-
-    app.put('/*', function(req, res) {
-        var d = '';
-        req.on('data', function(data) {
-            d += data;
-        });
-        req.on('end', function() {
-            node.set(req.params[0], d, function(err) {
-                if (err) console.log(err);
-                res.send(d);
-            });
-        });
-    });
-
-    app.listen(12121);
 });
+
+app.put('/*', function(req, res) {
+    var d = '';
+    req.on('data', function(data) {
+        d += data;
+    });
+    req.on('end', function() {
+        node.set(req.params[0], d, function(err) {
+            if (err) {
+                res.statusCode = (err.code == 'NOTFOUND' ? 404 : 500);
+                res.send(JSON.stringify(err));
+            } else {
+                res.send(d);
+            }
+        });
+    });
+});
+
+app.listen(12121);
